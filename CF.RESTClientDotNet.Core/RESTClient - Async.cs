063b1357f6bc3c3298646994ae4106ef9cc084d4
Christian <christian>
2016-08-13 13:53:18 +1000

    Smash a couple of files together

--- CF.RESTClientDotNet.Core/RESTClient - Async.cs
+++ CF.RESTClientDotNet.Core/RESTClient - Async.cs
@@ -8,6 +8,31 @@ namespace CF.RESTClientDotNet
 {
     public partial class RESTClient
     {
+        #region Fields
+        private Dictionary<string, string> _Headers = new Dictionary<string, string>();
+        #endregion
+
+        #region Public Properties 
+        public Type ErrorType { get; set; }
+        public int TimeoutMilliseconds { get; set; } = 10000;
+        public bool ReadToEnd { get; set; } = true;
+        public static ISerializationAdapter SerializationAdapter { get; set; }
+        public Uri BaseUri { get; set; }
+
+        public Dictionary<string, string> Headers
+        {
+            get
+            {
+                return _Headers;
+            }
+        }
+
+        #endregion
+
+        #region Public Static Properties
+        private static List<Type> PrimitiveTypes { get; } = new List<Type> { typeof(string), typeof(int), typeof(Guid), typeof(long), typeof(byte), typeof(char) };
+        #endregion
+
         #region Constructor
 
         public RESTClient(ISerializationAdapter serializationAdapter, Uri baseUri)
