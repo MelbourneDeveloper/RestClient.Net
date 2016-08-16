1dff503039310fa339fb06d7d63da8dcc5c82bf4
Christian <christian>
2016-08-17 07:38:30 +1000

    Start file restructure Setup the correct assembly info data

--- Shared/src/AssemblyInfo.cs
+++ Shared/src/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RESTClientDotNet.Core")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyCompany("Christian Findlay")]
+[assembly: AssemblyProduct("RESTClient.NET")]
+[assembly: AssemblyCopyright("Copyright © 2016 Christian Findlay")]
 [assembly: CLSCompliant(true)]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
