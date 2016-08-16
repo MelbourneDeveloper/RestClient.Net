0fe8c504ed27752597bd8b9ad0a57f3a15c9dcd2
Christian <christian>
2016-08-17 07:53:36 +1000

    Project renaming

--- Shared/src/AssemblyInfo.cs
+++ Shared/src/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -11,10 +12,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("RESTClient.NET")]
 [assembly: AssemblyCopyright("Copyright © 2016 Christian Findlay")]
 [assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8171457b-9ea1-4893-985b-f241918f57af")]
+//TODO: Is this necessary? Should each assembly be unique?
+//[assembly: Guid("8171457b-9ea1-4893-985b-f241918f57af")]
