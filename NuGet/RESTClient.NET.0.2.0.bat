95d8835ae66de41c5c424d120a0eee7bde819e78
Christian <christian>
2016-08-17 08:08:58 +1000

    Almost uniform

--- NuGet/RESTClient.NET.0.2.0.bat
+++ NuGet/RESTClient.NET.0.2.0.bat
@@ -1,2 +1,2 @@
 cd "C:\Temp\RESTClientDotNet\NuGet\Packaged"
-"c:\temp\nuget" pack "C:\Temp\RESTClientDotNet\NuGet\Exported\0.1.0\RESTClientDotNet.0.1.0.nuspec"
+"c:\temp\nuget" pack "C:\Temp\RESTClientDotNet\NuGet\Exported\0.2.0\RESTClient.NET.0.1.0.nuspec"
