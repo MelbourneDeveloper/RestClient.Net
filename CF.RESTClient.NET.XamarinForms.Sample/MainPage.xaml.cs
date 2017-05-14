bf1868b502067f48b27a8d216034329c85dd017b
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-05-14 14:11:46 +1000

    Get the main page compiling

--- CF.RESTClient.NET.XamarinForms.Sample/MainPage.xaml.cs
+++ CF.RESTClient.NET.XamarinForms.Sample/MainPage.xaml.cs
@@ -1,54 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms.Xaml;
 
 namespace CF.RESTClient.NET.XamarinForms.Sample
 {
 
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage 
     {
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = new ContentPageViewModel();
         }
-    }
-
-    class MainPageViewModel : INotifyPropertyChanged
-    {
-
-        public MainPageViewModel()
-        {
-            IncreaseCountCommand = new Command(IncreaseCount);
-        }
-
-        int count;
-
-        string countDisplay = "You clicked 0 times.";
-        public string CountDisplay
-        {
-            get { return countDisplay; }
-            set { countDisplay = value; OnPropertyChanged(); }
-        }
-
-        public ICommand IncreaseCountCommand { get; }
-
-        void IncreaseCount() =>
-            CountDisplay = $"You clicked {++count} times";
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
     }
 }
