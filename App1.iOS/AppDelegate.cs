b8a8d1f0d8c84a1c16166d5ff427c3ac06eaa1a6
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-05-14 15:00:41 +1000

    iOS App compiling

--- App1.iOS/AppDelegate.cs
+++ App1.iOS/AppDelegate.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using CF.RESTClient.NET.Sample;
 
 namespace App1.iOS
 {
--- CF.RESTClient.NET.Sample.Android/MainActivity.cs
+++ CF.RESTClient.NET.Sample.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using CF.RESTClient.NET.Sample;
 
 namespace App1.Droid
 {
-    [Activity(Label = "App1", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "RESTClient.NET Sample", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
