29c75967e13e6c0207365471c2837133fdaa0298
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-24 14:36:37 +1000

    .NET Sample getting closer

---
+++ CF.RESTClient.NET.Sample.NET/MainPage - DotNet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.RESTClient.NET.Sample
+{
+    public partial class MainPage
+    {
+    }
+}
