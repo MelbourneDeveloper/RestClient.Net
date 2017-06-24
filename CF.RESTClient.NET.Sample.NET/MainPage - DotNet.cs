50fea2dcd4fba2cd23482c89c0842710f682828d
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-24 14:41:59 +1000

    Some steps forward

--- CF.RESTClient.NET.Sample.NET/MainPage - DotNet.cs
+++ CF.RESTClient.NET.Sample.NET/MainPage - DotNet.cs
@@ -8,5 +8,19 @@ namespace CF.RESTClient.NET.Sample
 {
     public partial class MainPage
     {
+        private void ToggleReposBusy(bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task DisplayAlert(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AttachEventHandlers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
