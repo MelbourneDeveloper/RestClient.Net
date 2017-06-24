656528e2b13d376871da53c1b2c55d7ac362c815
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-24 14:30:54 +1000

    Renaming etc.

---
+++ CF.RESTClient.NET.Sample.NET/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace CF.RESTClient.NET.Sample.NET
+{
+
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
