0d9597424ee7baea6f26e6b398c058d010a9f3b4
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-04-28 14:33:16 +1000

    Getting the samples going again

---
+++ RESTClient.NET.Samples/RESTClient.NET.Samples/SerializationAdapters/NewtonsoftSerializationAdapter.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.RESTClientDotNet
+{
+    public class NewtonsoftSerializationAdapter : ISerializationAdapter
+    {
+        #region Public Properties
+        public Encoding Encoding { get; set; } = Encoding.UTF8;
+        #endregion
+
+        #region Implementation
+        public async Task<T> DeserializeAsync<T>(byte[] binary)
+        {
+            var markup = Encoding.GetString(binary);
+
+            object markupAsObject = markup;
+
+            if (typeof(T) == typeof(string))
+            {
+                return (T)markupAsObject;
+            }
+
+            var retVal = await Task.Run(() => JsonConvert.DeserializeObject<T>(markup));
+
+            return retVal;
+        }
+
+        public async Task<byte[]> SerializeAsync<T>(T value)
+        {
+            var json = await Task.Run(() => JsonConvert.SerializeObject(value));
+            var binary = await Task.Run(() => Encoding.GetBytes(json));
+            return binary;
+        }
+        #endregion
+    }
+}
