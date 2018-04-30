6f0108f67d5d4cf876678786d2da7eda6c46da7f
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-05-01 08:04:12 +1000

    .NET Core sample

--- RESTClient.NET.Samples/RESTClient.NET.CoreSample/Program.cs
+++ RESTClient.NET.Samples/RESTClient.NET.CoreSample/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Atlassian;
+using CF.RESTClientDotNet;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using restclientdotnet = CF.RESTClientDotNet;
 
 namespace RESTClient.NET.CoreSample
 {
     class Program
     {
+        static string username = "MelbourneDeveloper";
+        static string password = "";
+        static restclientdotnet.RESTClient _BitbucketClient;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Go();
+            Console.ReadLine();
+        }
+
+        private async static Task Go()
+        {
+            GetBitBucketClient(true);
+            var repos = (await _BitbucketClient.GetAsync<RepositoryList>());
+            var backup = repos.values[3];
+            backup.description = "testdesc";
+            backup = await _BitbucketClient.PostAsync<Repository, Repository>(backup, backup.full_name.Split('/')[1]);
+        }
+
+        private static void GetBitBucketClient(bool isGet)
+        {
+            var url = "https://api.bitbucket.org/2.0/repositories/" + username;
+            _BitbucketClient = new restclientdotnet.RESTClient(new NewtonsoftSerializationAdapter(), new Uri(url));
+
+            if (!string.IsNullOrEmpty(password))
+            {
+                var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(username + ":" + password));
+                _BitbucketClient.Headers.Add("Authorization", "Basic " + credentials);
+            }
+
+            _BitbucketClient.ErrorType = typeof(ErrorModel);
         }
     }
 }
