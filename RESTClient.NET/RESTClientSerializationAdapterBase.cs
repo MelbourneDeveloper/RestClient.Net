b08375ca1b46b1c8120cd31587f8ec7844bd3958
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-06-04 19:28:46 +1000

    Refactoring

---
+++ RESTClient.NET/RESTClientSerializationAdapterBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace CF.RESTClientDotNet
+{
+    public abstract  class RESTClientSerializationAdapterBase
+    {
+        #region Public Properties
+        public Encoding Encoding { get; set; } = Encoding.UTF8;
+        #endregion
+
+        #region Protected Methods
+        protected string GetMarkup(byte[] data)
+        {
+            return Encoding.GetString(data);
+        }
+        #endregion
+    }
+}
