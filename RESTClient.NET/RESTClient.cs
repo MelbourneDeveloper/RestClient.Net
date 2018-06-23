95032a8b91511cf48708488879a85cac75dcbcf7
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-06-23 16:57:37 +1000

    Crunch

--- RESTClient.NET/RESTClient.cs
+++ RESTClient.NET/RESTClient.cs
@@ -1,45 +1,39 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace CF.RESTClientDotNet
+namespace CryptoCurrency.Net
 {
     public class RESTClient
     {
         #region Fields
-        private readonly HttpClient _HttpClient = new HttpClient { Timeout = new TimeSpan(0, 3, 0) };
+        private readonly HttpClient _HttpClient = new HttpClient();
         #endregion
 
         #region Public Properties
         public Uri BaseUri => _HttpClient.BaseAddress;
-        public Dictionary<string, string> Headers { get; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Headers { get; private set; } = new Dictionary<string, string>();
         public AuthenticationHeaderValue Authorization { get; set; }
         public Dictionary<HttpStatusCode, Func<string, object>> HttpStatusCodeFuncs = new Dictionary<HttpStatusCode, Func<string, object>>();
         public IZip Zip;
-        public ISerializationAdapter SerializationAdapter { get; }
-        public TimeSpan Timeout
-        {
-            get => _HttpClient.Timeout;
-            set => _HttpClient.Timeout = value;
-        }
-        public ITracer Tracer { get; set; }
-        public Type ErrorType { get; set; }
         #endregion
 
         #region Constructor
-        public RESTClient(ISerializationAdapter serializationAdapter, Uri baseUri)
+        public RESTClient(Uri baseUri)
         {
             _HttpClient.BaseAddress = baseUri;
-            SerializationAdapter = serializationAdapter;
+            _HttpClient.Timeout = new TimeSpan(0, 3, 0);
         }
         #endregion
 
         #region Private Methods
-        private async Task<TReturn> Call<TReturn, TBody>(string queryString, HttpVerb httpVerb, string contentType, TBody body = default(TBody))
+        private async Task<T> Call<T>(string queryString, bool isPost, string contentType, object body = null)
         {
             _HttpClient.DefaultRequestHeaders.Clear();
 
@@ -48,8 +42,8 @@ namespace CF.RESTClientDotNet
                 _HttpClient.DefaultRequestHeaders.Authorization = Authorization;
             }
 
-            HttpResponseMessage result = null;
-            if (httpVerb != HttpVerb.Post)
+            HttpResponseMessage result;
+            if (!isPost)
             {
                 _HttpClient.DefaultRequestHeaders.Clear();
                 foreach (var key in Headers.Keys)
@@ -57,47 +51,28 @@ namespace CF.RESTClientDotNet
                     _HttpClient.DefaultRequestHeaders.Add(key, Headers[key]);
                 }
 
-                switch (httpVerb)
-                {
-                    case HttpVerb.Get:
-                        result = await _HttpClient.GetAsync(queryString);
-                        break;
-                    case HttpVerb.Put:
-
-                        var data = await SerializationAdapter.SerializeAsync(body);
-                        var bodyString = SerializationAdapter.Encoding.GetString(data);
-                        var length = bodyString.Length;
-                        var stringContent = new StringContent(bodyString, SerializationAdapter.Encoding, contentType);
-
-                        result = await _HttpClient.PutAsync(queryString, stringContent);
-                        break;
-                }
 
                 result = await _HttpClient.GetAsync(queryString);
             }
             else
             {
-                StringContent stringContent = null;
-                long length = 0;
+                string bodyString;
 
-                var bodyAsString = body as string;
-                if (bodyAsString != null)
+                if (body is string bodyAsString)
                 {
-                    stringContent = new StringContent(bodyAsString, SerializationAdapter.Encoding, contentType);
-                    length = bodyAsString.Length;
+                    bodyString = bodyAsString;
                 }
                 else
                 {
-                    var data = await SerializationAdapter.SerializeAsync(body);
-                    var bodyString = SerializationAdapter.Encoding.GetString(data);
-                    length = bodyString.Length;
-                    stringContent = new StringContent(bodyString, SerializationAdapter.Encoding, contentType);
+                    bodyString = body != null ? JsonConvert.SerializeObject(body) : string.Empty;
                 }
+
+                var stringContent = new StringContent(bodyString, Encoding.UTF8, contentType);
 
                 //Don't know why but this has to be set again, otherwise more text is added on to the Content-Type header...
                 stringContent.Headers.ContentType = new MediaTypeHeaderValue(contentType);
 
-                stringContent.Headers.ContentLength = length;
+                stringContent.Headers.ContentLength = bodyString.Length;
 
                 foreach (var key in Headers.Keys)
                 {
@@ -105,69 +80,56 @@ namespace CF.RESTClientDotNet
                 }
 
                 result = await _HttpClient.PostAsync(queryString, stringContent);
-
-                System.Diagnostics.Debug.WriteLine($"{_HttpClient.BaseAddress} OK");
             }
 
             if (result.IsSuccessStatusCode)
             {
                 var gzipHeader = result.Content.Headers.ContentEncoding.FirstOrDefault(h => !string.IsNullOrEmpty(h) && h.Equals("gzip", StringComparison.InvariantCultureIgnoreCase));
-                byte[] data;
+                string json;
                 if (gzipHeader != null && Zip != null)
                 {
                     var bytes = await result.Content.ReadAsByteArrayAsync();
                     var jsonUnzipped = Zip.Unzip(bytes);
 
-                    data = jsonUnzipped;
+                    //TODO: Big assumption of UTF8 here.
+                    json = Encoding.UTF8.GetString(jsonUnzipped);
                 }
                 else
                 {
-                    data = await result.Content.ReadAsByteArrayAsync();
+                    //TODO: This should be binary and we should use adapters like the old version of this lib so that it can actually be rereleased
+                    json = await result.Content.ReadAsStringAsync();
                 }
 
-                return await SerializationAdapter.DeserializeAsync<TReturn>(data);
+                if (typeof(T) != typeof(string))
+                {
+                    return JsonConvert.DeserializeObject<T>(json);
+                }
+
+                //Just return the string
+                object jsonAsObject = json;
+                return (T)jsonAsObject;
             }
+
+            var text = await result.Content.ReadAsStringAsync();
 
             if (HttpStatusCodeFuncs.ContainsKey(result.StatusCode))
             {
-                var text = await result.Content.ReadAsStringAsync();
-                return (TReturn)HttpStatusCodeFuncs[result.StatusCode].Invoke(text);
+                return (T)HttpStatusCodeFuncs[result.StatusCode].Invoke(text);
             }
-            else
-            {
-                var responseData = await result.Content.ReadAsByteArrayAsync();
-                if (ErrorType != null)
-                {
-                    var error = await SerializationAdapter.DeserializeAsync(responseData, ErrorType);
-                    throw new RESTException(error, responseData, "An error occurred. Please see Error property of this exception", result.StatusCode);
-                }
-                else
-                {
-                    throw new Exception($"An error occurred\r\n\r\n{SerializationAdapter.Encoding.GetString(responseData)}");
-                }
-            }
+
+            throw new HttpStatusException($"Nope {result.StatusCode}.\r\nBase Uri: {_HttpClient.BaseAddress}. Full Uri: {_HttpClient.BaseAddress + queryString}\r\nError:\r\n{text}", result.StatusCode);
         }
         #endregion
 
         #region Public Methods
-        public async Task<T> GetAsync<T>()
+        public async Task<T> GetAsync<T>(string queryString, string contentType = "application/json")
         {
-            return await GetAsync<T>(null);
-        }
-
-        public async Task<TReturn> GetAsync<TReturn>(string queryString, string contentType = "application/json")
-        {
-            return await Call<TReturn, object>(queryString, HttpVerb.Get, contentType);
+            return await Call<T>(queryString, false, contentType);
         }
 
         public async Task<TReturn> PostAsync<TReturn, TBody>(TBody body, string queryString, string contentType = "application/json")
         {
-            return await Call<TReturn, TBody>(queryString, HttpVerb.Post, contentType, body);
-        }
-
-        public async Task<TReturn> PutAsync<TReturn, TBody>(TBody body, string queryString, string contentType = "application/json")
-        {
-            return await Call<TReturn, TBody>(queryString, HttpVerb.Put, contentType, body);
+            return await Call<TReturn>(queryString, true, contentType, body);
         }
         #endregion
     }
