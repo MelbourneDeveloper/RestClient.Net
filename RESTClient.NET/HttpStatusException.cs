2f4d0ced5000f18c02f869939161375254f12d1a
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-06-23 17:17:35 +1000

    Add missing file

---
+++ RESTClient.NET/HttpStatusException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace CF.RESTClientDotNet
+{
+    public class HttpStatusException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+
+        public HttpStatusException(string message, HttpStatusCode statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
