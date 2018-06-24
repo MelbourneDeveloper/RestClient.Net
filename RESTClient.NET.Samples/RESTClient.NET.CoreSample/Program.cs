8250f9c6401c0dbbd1861d918a9ead4041450c55
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-06-24 11:45:58 +1000

    Delete all the junk, clean up the core sample

--- RESTClient.NET.Samples/RESTClient.NET.CoreSample/Program.cs
+++ RESTClient.NET.Samples/RESTClient.NET.CoreSample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Atlassian;
 using CF.RESTClientDotNet;
 using System;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using restclientdotnet = CF.RESTClientDotNet;
@@ -9,26 +10,44 @@ namespace RESTClient.NET.CoreSample
 {
     class Program
     {
-        static string username = "MelbourneDeveloper";
-        static string password = "";
-        static restclientdotnet.RESTClient _BitbucketClient;
+        #region Fields
+        //TODO: Change these values to what you want them to be
+        private const string RepoName = "Backup";
+        private const string RepoDescription = "Some description";
+        private static string username = "MelbourneDeveloper";
+        private static string password = "";
+        private static restclientdotnet.RESTClient _BitbucketClient;
+        #endregion
 
+        #region Main Method
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Go();
             Console.ReadLine();
         }
+        #endregion
 
+        #region Methods
         private async static Task Go()
         {
             GetBitBucketClient(true);
+
+            //Load the repos
             var repos = (await _BitbucketClient.GetAsync<RepositoryList>());
-            var backup = repos.values[3];
-            backup.description = "smooper";
-            var requestUri = $"https://api.bitbucket.org/2.0/repositories/{username}/{backup.full_name.Split('/')[1]}";
-            backup = await _BitbucketClient.PutAsync<Repository, Repository>(backup, requestUri);
-            System.Diagnostics.Debug.WriteLine(backup.description);
+
+            Console.WriteLine($"Got {repos.values.Count} repos.");
+
+            //Get the repo by name
+            var repo = repos.values.FirstOrDefault(r => r.name == RepoName);
+
+            repo.description = RepoDescription;
+
+            var requestUri = $"https://api.bitbucket.org/2.0/repositories/{username}/{repo.full_name.Split('/')[1]}";
+
+            //Save the repo with the new description and get the repo back from Bitbucket
+            repo = await _BitbucketClient.PutAsync<Repository, Repository>(repo, requestUri);
+
+            Console.WriteLine($"Saved repo and Bitbucket didn't complain. The description of the repo that came back from Bitbucket was '{repo.description}'.\r\n{(repo.description == RepoDescription ? "Changing the description succeeded." : "Changing the description failed.")}");
         }
 
         private static void GetBitBucketClient(bool isGet)
@@ -42,5 +61,6 @@ namespace RESTClient.NET.CoreSample
                 _BitbucketClient.Headers.Add("Authorization", "Basic " + credentials);
             }
         }
+        #endregion
     }
 }
