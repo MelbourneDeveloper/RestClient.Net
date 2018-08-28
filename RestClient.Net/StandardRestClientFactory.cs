4f2811f7d1ae312ad509ecab61d917249e61c6de
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-08-28 20:08:21 +1000

    Lust hump?

---
+++ RestClient.Net/StandardRestClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RestClientDotNet
+{
+    public class StandardRestClientFactory : IRestClientFactory
+    {
+        public ISerializationAdapter SerializationAdapter { get; }
+
+        public StandardRestClientFactory(ISerializationAdapter serializationAdapter)
+        {
+            SerializationAdapter = serializationAdapter;
+        }
+
+        public RestClient CreateRESTClient(Uri baseUri)
+        {
+            return new RestClient(SerializationAdapter, baseUri);
+        }
+    }
+}
