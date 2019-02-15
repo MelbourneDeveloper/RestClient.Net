6327f445fc39f19adbec24ad7e13747f32861d1f
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-02-15 19:43:55 +1100

    More test

--- RestClient.Net.UnitTests/PerformanceTests.cs
+++ RestClient.Net.UnitTests/PerformanceTests.cs
@@ -1,5 +1,6 @@
 ﻿using groupkt;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestClient.Net.Samples.Model;
 using RestClientDotNet;
 using RestSharp;
 using System;
@@ -11,7 +12,7 @@ namespace RestClient.Net.UnitTests
     public class PerformanceTests
     {
         [TestMethod]
-        public async Task PerformanceTestOne()
+        public async Task GetPerformanceTest()
         {
             var countryCodeClient = new RestClientDotNet.RestClient(new NewtonsoftSerializationAdapter(), new Uri("http://services.groupkt.com/country/get/all"));
 
@@ -38,7 +39,7 @@ namespace RestClient.Net.UnitTests
             {
                 var taskCompletionSource = new TaskCompletionSource<groupktResult<CountriesResult>>();
                 var response = restSharpClient.ExecuteAsync<groupktResult<CountriesResult>>(request, (a) => { taskCompletionSource.SetResult(a.Data); });
-                var result = await taskCompletionSource.Task;
+                countryData = await taskCompletionSource.Task;
             }
 
             var restSharpTotalMilliseconds = (DateTime.Now - startTime).TotalMilliseconds;
@@ -48,5 +49,46 @@ namespace RestClient.Net.UnitTests
 
             Console.WriteLine("🏆 RestClient Wins!!!");
         }
+
+        [TestMethod]
+        public async Task PatchPerformanceTest()
+        {
+
+            var restClient = new RestClientDotNet.RestClient(new NewtonsoftSerializationAdapter(), new Uri("https://jsonplaceholder.typicode.com"));
+
+            UserPost userPost = null;
+
+            var startTime = DateTime.Now;
+
+            for (var i = 0; i < 15; i++)
+                 userPost = await restClient.PatchAsync<UserPost, UserPost>(new UserPost { title = "Moops" }, "/posts/1");
+
+            var restClientTotalMilliseconds = (DateTime.Now - startTime).TotalMilliseconds;
+            Console.WriteLine($"RestClient Get : Total Milliseconds:{ restClientTotalMilliseconds}");
+
+
+            startTime = DateTime.Now;
+            var restSharpClient = new RestSharp.RestClient("https://jsonplaceholder.typicode.com");
+
+            var request = new RestRequest(Method.PATCH)
+            {
+                Resource = "/posts/1"
+            };
+
+            for (var i = 0; i < 15; i++)
+            {
+                var taskCompletionSource = new TaskCompletionSource<UserPost>();
+                var response = restSharpClient.ExecuteAsync<UserPost>(request, (a) => { taskCompletionSource.SetResult(a.Data); });
+                userPost = await taskCompletionSource.Task;
+            }
+
+            var restSharpTotalMilliseconds = (DateTime.Now - startTime).TotalMilliseconds;
+            Console.WriteLine($"RestSharp Get : Total Milliseconds:{ restSharpTotalMilliseconds}");
+
+            Assert.IsTrue(restClientTotalMilliseconds < restSharpTotalMilliseconds, "😞 RestSharp wins.");
+
+            Console.WriteLine("🏆 RestClient Wins!!!");
+        }
+
     }
 }
