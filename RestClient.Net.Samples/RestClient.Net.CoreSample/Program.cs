9fa480cf8dade9c76e3d83b3b835c71d0459fec2
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-20 21:02:12 +1100

    Almost working?

--- RestClient.Net.Samples/RestClient.Net.CoreSample/Program.cs
+++ RestClient.Net.Samples/RestClient.Net.CoreSample/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestClientApiSamples;
 
 namespace RESTClient.NET.CoreSample
 {
@@ -21,7 +22,7 @@ namespace RESTClient.NET.CoreSample
         private static async Task Go()
         {
             var restClient = new RestClient(new ProtobufSerializationAdapter(), new Uri("http://localhost:42908/person"));
-            //restClient.GetAsync < Person>
+            var person = restClient.GetAsync<Person>();
 
             Console.WriteLine($"Saved repo and Bitbucket didn't complain. The description of the repo that came back from Bitbucket was '{repo.description}'.\r\n{(repo.description == RepoDescription ? "Changing the description succeeded." : "Changing the description failed.")}");
         }
