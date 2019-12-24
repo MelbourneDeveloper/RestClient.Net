0eac3ab5933bf4d3f4f94dc57aa6bfcce482432f
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-24 15:17:13 +1100

    Cleanup

--- RestClient.Net.Abstractions/IRestClient.cs
+++ RestClient.Net.Abstractions/IRestClient.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Threading;
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RestClientDotNet.Abstractions
@@ -10,83 +6,9 @@ namespace RestClientDotNet.Abstractions
     public interface IRestClient
     {
         ISerializationAdapter SerializationAdapter { get; }
-        Task<RestResponse<TReturn>> SendAsync<TReturn, TBody>(RestRequest<TBody> restRequest);
+        Task<RestResponseBase<TReturn>> SendAsync<TReturn, TBody>(RestRequest<TBody> restRequest);
         IRestHeadersCollection DefaultRequestHeaders { get; }
         string DefaultContentType { get; }
         TimeSpan Timeout { get; set; }
-    }
-
-    public class RestRequest<TBody>
-    {
-
-        public RestRequest()
-        {
-
-        }
-
-        public RestRequest(IRestClient client) : this()
-        {
-            if (client == null) throw new ArgumentNullException(nameof(client));
-
-            var contentType = client.DefaultContentType;
-            if (!string.IsNullOrEmpty(contentType))
-            {
-                ContentType = contentType;
-            }
-            var headers = client.DefaultRequestHeaders;
-            if (headers != null)
-            {
-                foreach (var header in headers)
-                {
-                    Headers.Add(header.Key, header.Value);
-                }
-            }
-        }
-
-        public IRestHeadersCollection Headers { get; } = new RestRequestHeadersCollection();
-
-        public Uri Resource { get; set; }
-        public HttpVerb HttpVerb { get; set; } = HttpVerb.Get;
-        public string ContentType { get; set; } = "application/json";
-        public TBody Body { get; set; }
-
-
-    }
-
-    public class RestRequestHeadersCollection : IRestHeadersCollection
-    {
-#pragma warning disable CA1065 // Do not raise exceptions in unexpected locations
-        public IEnumerable<string> this[string name] => throw new NotImplementedException();
-#pragma warning restore CA1065 // Do not raise exceptions in unexpected locations
-
-        public void Add(string name, string value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Add(string name, IEnumerable<string> values)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerator<KeyValuePair<string, IEnumerable<string>>> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
---
+++ RestClient.Net.Abstractions/RestRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RestClientDotNet.Abstractions
+{
+    public class RestRequest<TBody>
+    {
+        #region Public Properties
+        public IRestHeadersCollection Headers { get; }
+        public Uri Resource { get; set; }
+        public HttpVerb HttpVerb { get; set; } = HttpVerb.Get;
+        public string ContentType { get; set; } = "application/json";
+        public TBody Body { get; set; }
+        #endregion
+
+        public RestRequest(TBody body, IRestHeadersCollection headers, IRestClient client)
+        {
+            Body = body;
+            Headers = headers ?? throw new ArgumentNullException(nameof(headers));
+
+            if (client == null) return;
+
+            var contentType = client.DefaultContentType;
+            if (!string.IsNullOrEmpty(contentType))
+            {
+                ContentType = contentType;
+            }
+
+            var clientHeaders = client.DefaultRequestHeaders;
+            if (clientHeaders != null)
+            {
+                foreach (var header in clientHeaders)
+                {
+                    Headers.Add(header.Key, header.Value);
+                }
+            }
+        }
+    }
+}
