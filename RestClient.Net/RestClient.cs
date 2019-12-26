04be890f05eac572b2253f7e7b38a2e2cb902abd
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-26 13:53:36 +1100

    Refactor

--- RestClient.Net/RestClient.cs
+++ RestClient.Net/RestClient.cs
@@ -98,11 +98,17 @@ namespace RestClientDotNet
         {
             var httpClient = HttpClientFactory.CreateClient(Name);
 
+            //TODO: This is unnecessary in the Singleton scenario
+            //TODO: In other scenarios this is no good because the client may be used to hit two different Uris at the same time and this will break it
+            //Possible solution: The HttpClientFactory could return a flag indicating if the client is fresh...
             httpClient.DefaultRequestHeaders.Clear();
             foreach (var name in DefaultRequestHeaders.Names)
             {
                 httpClient.DefaultRequestHeaders.Add(name, DefaultRequestHeaders[name]);
             }
+
+            if (httpClient.Timeout != Timeout && Timeout != default) httpClient.Timeout = Timeout;
+            if (BaseUri != null) httpClient.BaseAddress = BaseUri;
 
             HttpMethod httpMethod;
             switch (restRequest.HttpVerb)
@@ -126,35 +132,33 @@ namespace RestClientDotNet
                     throw new NotImplementedException();
             }
 
-            //TODO
-#pragma warning disable CA2000 // Dispose objects before losing scope
-            var httpRequestMessage = new HttpRequestMessage
+            using (var httpRequestMessage = new HttpRequestMessage
             {
                 Method = httpMethod,
                 RequestUri = restRequest.Resource
-            };
-#pragma warning restore CA2000 // Dispose objects before losing scope
-
-            byte[] requestBodyData = null;
-            if (new List<HttpVerb> { HttpVerb.Put, HttpVerb.Post, HttpVerb.Patch }.Contains(restRequest.HttpVerb))
+            })
             {
-                requestBodyData = await SerializationAdapter.SerializeAsync(restRequest.Body);
-                var httpContent = new ByteArrayContent(requestBodyData);
-                //Why do we have to set the content type only in cases where there is a request restRequest.Body, and headers?
-                httpContent.Headers.Add("Content-Type", restRequest.ContentType);
-                httpRequestMessage.Content = httpContent;
+                byte[] requestBodyData = null;
+                if (new List<HttpVerb> { HttpVerb.Put, HttpVerb.Post, HttpVerb.Patch }.Contains(restRequest.HttpVerb))
+                {
+                    requestBodyData = await SerializationAdapter.SerializeAsync(restRequest.Body);
+                    var httpContent = new ByteArrayContent(requestBodyData);
+                    //Why do we have to set the content type only in cases where there is a request restRequest.Body, and headers?
+                    httpContent.Headers.Add("Content-Type", restRequest.ContentType);
+                    httpRequestMessage.Content = httpContent;
+                }
+
+                foreach (var headerName in restRequest.Headers.Names)
+                {
+                    httpRequestMessage.Headers.Add(headerName, restRequest.Headers[headerName]);
+                }
+
+                Tracer?.Trace(restRequest.HttpVerb, httpClient.BaseAddress, restRequest.Resource, requestBodyData, TraceType.Request, null, restRequest.Headers);
+
+                var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage, restRequest.CancellationToken);
+
+                return await ProcessResponseAsync<TResponseBody, TRequestBody>(restRequest, httpClient, httpResponseMessage);
             }
-
-            foreach (var headerName in restRequest.Headers.Names)
-            {
-                httpRequestMessage.Headers.Add(headerName, restRequest.Headers[headerName]);
-            }
-
-            Tracer?.Trace(restRequest.HttpVerb, httpClient.BaseAddress, restRequest.Resource, requestBodyData, TraceType.Request, null, restRequest.Headers);
-
-            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage, restRequest.CancellationToken);
-
-            return await ProcessResponseAsync<TResponseBody, TRequestBody>(restRequest, httpClient, httpResponseMessage);
         }
 
         private async Task<RestResponseBase<TResponseBody>> ProcessResponseAsync<TResponseBody, TRequestBody>(RestRequest<TRequestBody> restRequest, HttpClient httpClient, HttpResponseMessage httpResponseMessage)
