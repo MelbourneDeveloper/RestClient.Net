276ec6309080cc214af49f3b7b1915209079f357
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-27 08:29:58 +1100

    Attempting to pass the exception message back to the client

---
+++ ApiExamples/Controllers/ExceptionController.cs
@@ -0,0 +1,25 @@
+﻿using ApiExamples.Model;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiExamples.Controllers
+{
+    [ApiController]
+    public class ExceptionController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            var json = JsonConvert.SerializeObject(new ApiResult { Errors = { context.Error.Message } });
+            return BadRequest(json);
+
+
+            //return Problem(
+            //    detail: context.Error.Message,
+            //    title: context.Error.Message);
+        }
+    }
+}
