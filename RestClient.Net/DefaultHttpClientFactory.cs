a1bc5ce3c9f48b9dcb032b7a28473671e912837e
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-26 15:21:32 +1100

    Add the class

---
+++ RestClient.Net/DefaultHttpClientFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Threading;
+
+namespace RestClientDotNet
+{
+    public class DefaultHttpClientFactory : IHttpClientFactory, IDisposable
+    {
+        #region Fields
+        private bool disposed;
+        private readonly ConcurrentDictionary<string, Lazy<HttpClient>> _httpClients;
+        private readonly Func<string, Lazy<HttpClient>> _getOrAddFunc;
+        #endregion
+
+        #region Constructor
+        public DefaultHttpClientFactory(Func<string, Lazy<HttpClient>> func)
+        {
+            _getOrAddFunc = func;
+            _httpClients = new ConcurrentDictionary<string, Lazy<HttpClient>>();            if (_getOrAddFunc != null) return;            _getOrAddFunc = (name) =>            {                return new Lazy<HttpClient>(() =>                {                    return MintClient();                }, LazyThreadSafetyMode.ExecutionAndPublication);            };
+        }
+        #endregion
+
+        #region Implementation
+        public HttpClient CreateClient(string name)
+        {
+            if (name == null)            {                throw new ArgumentNullException(nameof(name));            }            return _httpClients.GetOrAdd(name, _getOrAddFunc).Value;
+        }
+
+        public void Dispose()
+        {
+            if (disposed) return;
+            disposed = true;
+
+            foreach(var name in _httpClients.Keys)
+            {
+                _httpClients[name].Value.Dispose();
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private HttpClient MintClient()
+        {
+            return new HttpClient();
+        }
+        #endregion
+    }
+}
--- RestClient.Net/IHttpClientFactory.cs
+++ RestClient.Net/IHttpClientFactory.cs
@@ -7,7 +7,7 @@ namespace RestClientDotNet
     /// Generates HttpClients as necessary
     /// Base on this Microsoft interface which is only available in .Net Core 2.1 +: https://docs.microsoft.com/en-us/dotnet/api/system.net.http.ihttpclientfactory?view=dotnet-plat-ext-3.1
     /// </summary>
-    public interface IHttpClientFactory : IDisposable
+    public interface IHttpClientFactory
     {
         HttpClient CreateClient(string name);
     }
