377d7f0c234bab5ee16976704d032dfbc4c4cb98
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-26 13:22:53 +1100

    RestClient naming

--- RestClient.Net.Abstractions/IRestClientFactory.cs
+++ RestClient.Net.Abstractions/IRestClientFactory.cs
@@ -1,17 +1,7 @@
-﻿using System;
-
-namespace RestClientDotNet.Abstractions
+﻿namespace RestClientDotNet.Abstractions
 {
     public interface IRestClientFactory
     {
         IRestClient CreateRestClient(string name);
-    }
-
-    public static class RestClientFactoryExtensions
-    {
-        public static IRestClient CreateRestClient(this IRestClient restClient, Uri baseUri)
-        {
-            restClient.CreateRestClient("RestClient");
-        }
     }
 }
---
+++ RestClient.Net.Abstractions/RestClientFactoryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RestClientDotNet.Abstractions
+{
+    public static class RestClientFactoryExtensions
+    {
+        public static IRestClient CreateRestClient(this IRestClientFactory restClientFactory)
+        {
+            if (restClientFactory == null) throw new ArgumentNullException(nameof(restClientFactory));
+            return restClientFactory.CreateRestClient("RestClient");
+        }
+    }
+}
