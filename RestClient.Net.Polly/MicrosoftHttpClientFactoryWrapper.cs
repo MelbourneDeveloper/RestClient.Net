a518581b8f6d5fa0ae5d45f23dad49f17acb4891
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-30 14:09:05 +1100

    Create a wrapper for Microsoft's HttpClientFactory

---
+++ RestClient.Net.Polly/MicrosoftHttpClientFactoryWrapper.cs
@@ -0,0 +1,26 @@
+﻿using RestClientDotNet;
+using snh = System.Net.Http;
+
+namespace RestClient.Net.Polly
+{
+    public class MicrosoftHttpClientFactoryWrapper : IHttpClientFactory
+    {
+        #region Public Properties
+        snh.IHttpClientFactory HttpClientFactory { get; }
+        #endregion
+
+        #region Constructor
+        public MicrosoftHttpClientFactoryWrapper(snh.IHttpClientFactory httpClientFactory)
+        {
+            HttpClientFactory = httpClientFactory;
+        }
+        #endregion
+
+        #region Implementation
+        public snh.HttpClient CreateClient(string name)
+        {
+            return HttpClientFactory.CreateClient(name);
+        }
+        #endregion
+    }
+}
