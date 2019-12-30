208fa9e30be2a250ce517b82af067de7250a249f
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-30 14:51:48 +1100

    Revert some unnecessary changes

--- RestClient.Net/DefaultHttpClientFactory.cs
+++ RestClient.Net/DefaultHttpClientFactory.cs
@@ -21,14 +21,25 @@ namespace RestClientDotNet
         public DefaultHttpClientFactory(Func<string, Lazy<HttpClient>> func)
         {
             _getOrAddFunc = func;
-            _httpClients = new ConcurrentDictionary<string, Lazy<HttpClient>>();            if (_getOrAddFunc != null) return;            _getOrAddFunc = name =>            {                return new Lazy<HttpClient>(() => new HttpClient(), LazyThreadSafetyMode.ExecutionAndPublication);            };
+            _httpClients = new ConcurrentDictionary<string, Lazy<HttpClient>>();
+
+            if (_getOrAddFunc != null) return;
+            _getOrAddFunc = name =>
+            {
+                return new Lazy<HttpClient>(() => new HttpClient(), LazyThreadSafetyMode.ExecutionAndPublication);
+            };
         }
         #endregion
 
         #region Implementation
         public HttpClient CreateClient(string name)
         {
-            if (name == null)            {                throw new ArgumentNullException(nameof(name));            }            return _httpClients.GetOrAdd(name, _getOrAddFunc).Value;
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            return _httpClients.GetOrAdd(name, _getOrAddFunc).Value;
         }
 
         public void Dispose()
@@ -40,17 +51,6 @@ namespace RestClientDotNet
             {
                 _httpClients[name].Value.Dispose();
             }
-        }
-        #endregion
-
-        #region Public Methods
-        public void AddClient(string name, HttpClient httpClient)
-        {
-            var lazy = new Lazy<HttpClient>(() => httpClient);
-            var lazyFunc = new Func<string, Lazy<HttpClient>, Lazy<HttpClient>>((n, l) => lazy);
-#pragma warning disable IDE0058 // Expression value is never used
-            _httpClients.AddOrUpdate(name, lazy, lazyFunc);
-#pragma warning restore IDE0058 // Expression value is never used
         }
         #endregion
     }
--- RestClient.Net/IHttpClientFactory.cs
+++ RestClient.Net/IHttpClientFactory.cs
@@ -3,8 +3,8 @@
 namespace RestClientDotNet
 {
     /// <summary>
-    /// Generates HttpClients by name
-    /// Based on this Microsoft interface which is only available in .Net Core 2.1 +: https://docs.microsoft.com/en-us/dotnet/api/system.net.http.ihttpclientfactory?view=dotnet-plat-ext-3.1
+    /// Generates HttpClients as necessary
+    /// Base on this Microsoft interface which is only available in .Net Core 2.1 +: https://docs.microsoft.com/en-us/dotnet/api/system.net.http.ihttpclientfactory?view=dotnet-plat-ext-3.1
     /// </summary>
     public interface IHttpClientFactory
     {
