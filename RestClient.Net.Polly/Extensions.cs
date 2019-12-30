1d9a3d0f6e357567f618e02ee1713ee8c391a242
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-30 14:31:17 +1100

    Create an extension method

---
+++ RestClient.Net.Polly/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestClientDotNet;
+
+namespace RestClient.Net.Polly
+{
+    public static class Extensions
+    {
+        public static void AddDependencyInjectionMapping(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton(typeof(IHttpClientFactory), typeof(MicrosoftHttpClientFactoryWrapper));
+        }
+    }
+}
--- RestClient.Net.UnitTests/PollyDITests.cs
+++ RestClient.Net.UnitTests/PollyDITests.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestClient.Net.Polly;
 using System;
-using snh = System.Net.Http;
 
 namespace RestClientDotNet.UnitTests
 {
@@ -12,12 +11,12 @@ namespace RestClientDotNet.UnitTests
     public class PollyDITests
     {
         [TestMethod]
-        public void Test()
+        public void TestDIMapping()
         {
             var serviceCollection = new ServiceCollection();
             var baseUri = new Uri("http://www.test.com");
             serviceCollection.AddHttpClient("test", (c)=> { c.BaseAddress = baseUri; });
-            serviceCollection.AddSingleton(typeof(IHttpClientFactory), typeof(MicrosoftHttpClientFactoryWrapper));
+            serviceCollection.AddDependencyInjectionMapping();
             var serviceProvider = serviceCollection.BuildServiceProvider();
             var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
             var httpClient = httpClientFactory.CreateClient("test");
