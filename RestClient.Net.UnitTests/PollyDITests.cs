0912b5eed6b08785514f802e6a38e7671423fde0
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-30 14:01:27 +1100

    Get rid of the Microsoft dependency

--- RestClient.Net.UnitTests/PollyDITests.cs
+++ RestClient.Net.UnitTests/PollyDITests.cs
@@ -2,12 +2,8 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using StructureMap;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Net.Http;
-
 
 namespace RestClientDotNet.UnitTests
 {
@@ -22,70 +18,9 @@ namespace RestClientDotNet.UnitTests
             var asdasd = serviceCollection.BuildServiceProvider();
             var adaadf = asdasd.GetService<IHttpClientFactory>();
 
-
             var asdasds = adaadf.CreateClient("test");
 
-
         }
-    }
-
-    //public class asdasd : IServiceCollection
-    //{
-    //    public ServiceDescriptor this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-    //    public int Count => throw new NotImplementedException();
-
-    //    public bool IsReadOnly => throw new NotImplementedException();
-
-    //    public void Add(ServiceDescriptor item)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void Clear()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public bool Contains(ServiceDescriptor item)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void CopyTo(ServiceDescriptor[] array, int arrayIndex)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public IEnumerator<ServiceDescriptor> GetEnumerator()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public int IndexOf(ServiceDescriptor item)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void Insert(int index, ServiceDescriptor item)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public bool Remove(ServiceDescriptor item)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public void RemoveAt(int index)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    IEnumerator IEnumerable.GetEnumerator()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
+    }  
 }
 #endif
