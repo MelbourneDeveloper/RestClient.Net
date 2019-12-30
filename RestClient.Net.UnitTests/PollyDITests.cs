1bf30937bfe0a5c81b0e94e731125da51de26fbf
Christian Findlay <christian@zedtechnologies.com.au>
2019-12-30 14:18:11 +1100

    It all compiles

--- RestClient.Net.UnitTests/PollyDITests.cs
+++ RestClient.Net.UnitTests/PollyDITests.cs
@@ -2,8 +2,9 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestClient.Net.Polly;
 using System;
-using System.Net.Http;
+using snh = System.Net.Http;
 
 namespace RestClientDotNet.UnitTests
 {
@@ -13,8 +14,10 @@ namespace RestClientDotNet.UnitTests
         [TestMethod]
         public void Test()
         {
-            var serviceCollection = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+            var serviceCollection = new ServiceCollection();
             serviceCollection.AddHttpClient("test", (a)=> { a.BaseAddress = new Uri("http://www.test.com"); });
+            serviceCollection.AddSingleton(typeof(IHttpClientFactory), typeof(MicrosoftHttpClientFactoryWrapper));
+
             var asdasd = serviceCollection.BuildServiceProvider();
             var adaadf = asdasd.GetService<IHttpClientFactory>();
 
