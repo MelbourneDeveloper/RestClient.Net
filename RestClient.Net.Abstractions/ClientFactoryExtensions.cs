43e8ecfc9f187c93c50c6ba413c52cb783e43f8a
Christian Findlay <christian@zedtechnologies.com.au>
2020-01-23 08:06:32 +1100

    Add a test to make sure that unnamed instances end up being the same reference. Also comments

--- RestClient.Net.Abstractions/ClientFactoryExtensions.cs
+++ RestClient.Net.Abstractions/ClientFactoryExtensions.cs
@@ -4,6 +4,11 @@ namespace RestClient.Net.Abstractions
 {
     public static class ClientFactoryExtensions
     {
+        /// <summary>
+        /// Creates a client by the name of RestClient. Note: The factory will never create more than one IClient if this extension is used. The Client will act as a singleton, and only one HttpClient name will be used for the HttpClient factory. This should only be used for simple scenarios where the BaseUri is always the same and headers can be safely reused. 
+        /// </summary>
+        /// <param name="clientFactory"></param>
+        /// <returns>An instance of IClient that may be recycled from a previous call</returns>
         public static IClient CreateClient(this IClientFactory clientFactory)
         {
             //Use 'RestClient' by default because if this is null, the dictionary fails,
