5b0183170999ca45ea3ff853d6a90c3320047a6c
Christian Findlay <a@a.com>
2020-06-17 20:15:16 +1000

    Format the document

--- src/RestClient.Net.UnitTests/PollyTests.cs
+++ src/RestClient.Net.UnitTests/PollyTests.cs
@@ -49,48 +49,48 @@ namespace RestClient.Net.UnitTests
 
             var person = new Person { FirstName = "Bob", Surname = "Smith" };
 
-        //Note the Uri here is deliberately incorrect. It will cause a 404 Not found response. This is to make sure that polly is working
-        person = await client.PostAsync<Person, Person>(person, new Uri("person2", UriKind.Relative));
+            //Note the Uri here is deliberately incorrect. It will cause a 404 Not found response. This is to make sure that polly is working
+            person = await client.PostAsync<Person, Person>(person, new Uri("person2", UriKind.Relative));
             Assert.AreEqual("Bob", person.FirstName);
             Assert.AreEqual(3, tries);
 
         }
 
 
-    [TestMethod]
-    public async Task TestPollyWithDependencyInjection()
-    {
-        //Configure a Polly policy
-        var policy = HttpPolicyExtensions
-            .HandleTransientHttpError()
-            .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
-            .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        [TestMethod]
+        public async Task TestPollyWithDependencyInjection()
+        {
+            //Configure a Polly policy
+            var policy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 
-        //Create a Microsoft IoC Container
-        var serviceCollection = new ServiceCollection();
-        var baseUri = new Uri("https://restcountries.eu/rest/v2/");
-        serviceCollection.AddSingleton(typeof(ISerializationAdapter), typeof(NewtonsoftSerializationAdapter));
-        serviceCollection.AddSingleton(typeof(ILogger), typeof(ConsoleLogger));
+            //Create a Microsoft IoC Container
+            var serviceCollection = new ServiceCollection();
+            var baseUri = new Uri("https://restcountries.eu/rest/v2/");
+            serviceCollection.AddSingleton(typeof(ISerializationAdapter), typeof(NewtonsoftSerializationAdapter));
+            serviceCollection.AddSingleton(typeof(ILogger), typeof(ConsoleLogger));
 
-        //Add the Polly policy to the named HttpClient instance
-        serviceCollection.AddHttpClient("rc", (c) => { c.BaseAddress = baseUri; }).
-            SetHandlerLifetime(TimeSpan.FromMinutes(5)).
-            AddPolicyHandler(policy);
+            //Add the Polly policy to the named HttpClient instance
+            serviceCollection.AddHttpClient("rc", (c) => { c.BaseAddress = baseUri; }).
+                SetHandlerLifetime(TimeSpan.FromMinutes(5)).
+                AddPolicyHandler(policy);
 
-        //Provides mapping for Microsoft's IHttpClientFactory (This is what makes the magic happen)
-        serviceCollection.AddDependencyInjectionMapping();
+            //Provides mapping for Microsoft's IHttpClientFactory (This is what makes the magic happen)
+            serviceCollection.AddDependencyInjectionMapping();
 
-        var serviceProvider = serviceCollection.BuildServiceProvider();
-        var clientFactory = serviceProvider.GetService<CreateClient>();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var clientFactory = serviceProvider.GetService<CreateClient>();
 
-        //Create a Rest Client that will get the HttpClient by the name of rc
-        var client = clientFactory("rc");
+            //Create a Rest Client that will get the HttpClient by the name of rc
+            var client = clientFactory("rc");
 
-        //Make the call
-        var response = await client.GetAsync<List<RestCountry>>();
+            //Make the call
+            var response = await client.GetAsync<List<RestCountry>>();
 
-        //TODO: Implement this completely to ensure that the policy is being applied
+            //TODO: Implement this completely to ensure that the policy is being applied
+        }
+
     }
-
-}
 }
