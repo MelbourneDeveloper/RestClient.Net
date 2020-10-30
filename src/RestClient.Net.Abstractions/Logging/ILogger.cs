236a9d58e43577fef8d1d0b7bedf2578ce42b442
Christian Findlay <email@email.email>
2020-10-31 08:44:04 +1100

    Add generics on the ILogger

--- src/RestClient.Net.Abstractions/Logging/ILogger.cs
+++ src/RestClient.Net.Abstractions/Logging/ILogger.cs
@@ -7,6 +7,7 @@ namespace RestClient.Net.Abstractions.Logging
         void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter);
     }
 
+    public interface ILogger<T> : ILogger { }
 
 #pragma warning disable CA1815 // Override equals and operator equals on value types
 }
