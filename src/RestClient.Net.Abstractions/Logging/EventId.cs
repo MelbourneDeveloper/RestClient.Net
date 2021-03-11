b2af21512c37e2e6c4658c1f29abdacc98cd8d96
Christian Findlay <email@email.email>
2021-03-11 16:14:56 +1100

    Fix up event id

--- src/RestClient.Net.Abstractions/Logging/EventId.cs
+++ src/RestClient.Net.Abstractions/Logging/EventId.cs
@@ -4,13 +4,13 @@
     public readonly struct EventId
 #pragma warning restore CA1815 // Override equals and operator equals on value types
     {
-        public EventId(int id, string name = null)
+        public EventId(int id, string? name = null)
         {
             Id = id;
             Name = name;
         }
 
         public int Id { get; }
-        public string Name { get; }
+        public string? Name { get; }
     }
 }
