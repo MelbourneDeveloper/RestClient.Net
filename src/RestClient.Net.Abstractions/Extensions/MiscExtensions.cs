28bde8802c93ce008b450d7262c4721a0b0ea924
Christian Findlay <email@email.email>
2021-03-14 13:55:04 +1100

    Redo the header extensions

--- src/RestClient.Net.Abstractions/Extensions/MiscExtensions.cs
+++ src/RestClient.Net.Abstractions/Extensions/MiscExtensions.cs
@@ -10,46 +10,13 @@ namespace RestClient.Net.Abstractions.Extensions
 
         #region Internal Fields
 
+        internal const string Authorization = "Authorization";
         internal const string ContentTypeHeaderName = "Content-Type";
-
         internal const string JsonMediaType = "application/json";
 
         #endregion Internal Fields
 
-        #region Private Fields
-
-
-        #endregion Private Fields
-
         #region Public Methods
-
-        public static TResponseBody DeserializeResponseBody<TResponseBody>(this IClient restClient, byte[] response, IHeadersCollection headersCollection) => restClient == null
-                        ? throw new ArgumentNullException(nameof(restClient))
-                        : response == null
-                        ? throw new ArgumentNullException(nameof(response))
-                        : restClient.SerializationAdapter.Deserialize<TResponseBody>(response, headersCollection);
-
-        /// <summary>
-        /// Sets the Authorization header for Basic Authentication with the specified credentials
-        /// </summary>
-        public static IHeadersCollection SetBasicAuthenticationHeader(this IHeadersCollection requestHeaders, string userName, string password)
-        {
-            if (requestHeaders == null) throw new ArgumentNullException(nameof(requestHeaders));
-            var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(userName + ":" + password));
-            return requestHeaders.Append("Authorization", "Basic " + credentials);
-        }
-
-        public static IHeadersCollection SetBearerTokenAuthenticationHeader(string bearerToken) => SetBearerTokenAuthenticationHeader(new RequestHeadersCollection(new Dictionary<string, IEnumerable<string>>()), bearerToken);
-
-        public static IHeadersCollection SetBearerTokenAuthenticationHeader(this IHeadersCollection requestHeaders, string bearerToken)
-        {
-            return requestHeaders == null
-                ? throw new ArgumentNullException(nameof(requestHeaders))
-                : requestHeaders.Append("Authorization", "Bearer " + bearerToken);
-        }
-
-        public static IHeadersCollection SetJsonContentTypeHeader(this IHeadersCollection? requestHeaders)
-        => CreateOrSetHeaderValue(requestHeaders, ContentTypeHeaderName, ContentTypeHeaderName);
 
         public static IHeadersCollection CreateOrSetHeaderValue(
             this IHeadersCollection? requestHeaders,
@@ -66,6 +33,24 @@ namespace RestClient.Net.Abstractions.Extensions
                     }
                     )) :
             requestHeaders.Append(key, value);
+
+        public static TResponseBody DeserializeResponseBody<TResponseBody>(this IClient restClient, byte[] response, IHeadersCollection headersCollection) => restClient == null
+                                ? throw new ArgumentNullException(nameof(restClient))
+                        : response == null
+                        ? throw new ArgumentNullException(nameof(response))
+                        : restClient.SerializationAdapter.Deserialize<TResponseBody>(response, headersCollection);
+
+        /// <summary>
+        /// Sets the Authorization header for Basic Authentication with the specified credentials
+        /// </summary>
+        public static IHeadersCollection SetBasicAuthenticationHeader(this IHeadersCollection? requestHeaders, string userName, string password)
+            => CreateOrSetHeaderValue(requestHeaders, Authorization, "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(userName + ":" + password)));
+
+        public static IHeadersCollection SetBearerTokenAuthenticationHeader(this IHeadersCollection? requestHeaders, string bearerToken)
+            => CreateOrSetHeaderValue(requestHeaders, Authorization, "Bearer " + bearerToken);
+
+        public static IHeadersCollection SetJsonContentTypeHeader(this IHeadersCollection? requestHeaders)
+        => CreateOrSetHeaderValue(requestHeaders, ContentTypeHeaderName, ContentTypeHeaderName);
 
         #endregion Public Methods
 
