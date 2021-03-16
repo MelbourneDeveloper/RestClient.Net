eda509a6aa8ef0366b268984e381f65e799d7587
Christian Findlay <email@email.email>
2021-03-16 12:45:00 +1100

    MOre with stuff

--- src/RestClient.Net/ClientExtensions.cs
+++ src/RestClient.Net/ClientExtensions.cs
@@ -9,7 +9,23 @@ namespace RestClient.Net
 {
     public static class ClientExtensions
     {
+
         #region Public Methods
+
+        public static Client WithBaseUri(this Client client, Uri baseUri)
+        =>
+            client != null ? new Client(
+            client.SerializationAdapter,
+            client.Name,
+            baseUri,
+            client.DefaultRequestHeaders,
+            client.logger is ILogger<Client> logger ? logger : null,
+            client.createHttpClient,
+            client.sendHttpRequestFunc,
+            client.getHttpRequestMessage,
+            client.Timeout,
+            client.zip,
+            client.ThrowExceptionOnFailure) : throw new ArgumentNullException(nameof(client));
 
         public static Client WithDefaultHeaders(this Client client, IHeadersCollection defaultRequestHeaders)
         =>
@@ -29,8 +45,36 @@ namespace RestClient.Net
         public static Client WithDefaultHeaders(this Client client, string key, string value)
             => WithDefaultHeaders(client, key.CreateHeadersCollection(value));
 
+        public static Client WithLogger(this Client client, ILogger<Client> logger)
+        =>
+            client != null ? new Client(
+            client.SerializationAdapter,
+            client.Name,
+            client.BaseUri,
+            client.DefaultRequestHeaders,
+            logger,
+            client.createHttpClient,
+            client.sendHttpRequestFunc,
+            client.getHttpRequestMessage,
+            client.Timeout,
+            client.zip,
+            client.ThrowExceptionOnFailure) : throw new ArgumentNullException(nameof(client));
+
+        public static Client WithSerializationAdapter(this Client client, ISerializationAdapter serializationAdapter)
+                                        =>
+            client != null ? new Client(
+            serializationAdapter,
+            client.Name,
+            client.BaseUri,
+            client.DefaultRequestHeaders,
+            client.logger is ILogger<Client> logger ? logger : null,
+            client.createHttpClient,
+            client.sendHttpRequestFunc,
+            client.getHttpRequestMessage,
+            client.Timeout,
+            client.zip,
+            client.ThrowExceptionOnFailure) : throw new ArgumentNullException(nameof(client));
+
         #endregion Public Methods
-
-
     }
 }
