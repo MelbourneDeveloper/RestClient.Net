4cdee7a814202997c579ae495c1eaa5d1771a1a2
Christian Findlay <email@email.email>
2021-03-18 15:28:49 +1100

    Add another test

--- src/RestClient.Net.UnitTests/NullCheckTests.cs
+++ src/RestClient.Net.UnitTests/NullCheckTests.cs
@@ -1,29 +1,40 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using RestClient.Net.Abstractions;
 
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+
 namespace RestClient.Net.UnitTests
 {
     [TestClass]
-    public class NullCheckTests
+#pragma warning disable CA1063 // Implement IDisposable Correctly
+    public class NullCheckTests : IDisposable
+#pragma warning restore CA1063 // Implement IDisposable Correctly
     {
+        private readonly Mock<IGetHttpRequestMessage> getHttpRequestMessageMock = new Mock<IGetHttpRequestMessage>();
+        private readonly Mock<IRequest<string>> request = new Mock<IRequest<string>>();
+        private readonly Mock<ILogger<Client>> logger = new Mock<ILogger<Client>>();
+        private readonly Mock<ISerializationAdapter> serializationAdapterMock = new Mock<ISerializationAdapter>();
+        private readonly HttpClient httpClient = new HttpClient();
+
+#pragma warning disable CA1063 // Implement IDisposable Correctly
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+        public void Dispose() => httpClient.Dispose();
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+#pragma warning restore CA1063 // Implement IDisposable Correctly
+
         [TestMethod]
         public async Task TestDefaultSendHttpRequestMessagehttpClient()
         {
-            var getHttpRequestMessageMock = new Mock<IGetHttpRequestMessage>();
-            var request = new Mock<IRequest<string>>();
-            var logger = new Mock<ILogger<Client>>();
-            var serializationAdapterMock = new Mock<ISerializationAdapter>();
 
             var exception = await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () =>
             {
                 _ = await DefaultSendHttpRequestMessage.Instance.SendHttpRequestMessage(
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
                     null,
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
                     getHttpRequestMessageMock.Object,
                     request.Object,
                     logger.Object,
@@ -32,6 +43,24 @@ namespace RestClient.Net.UnitTests
             }).ConfigureAwait(false);
 
             Assert.AreEqual("httpClient", exception.ParamName);
+        }
+
+        [TestMethod]
+        public async Task TestDefaultSendHttpRequestMessagehttpRequestMessageFunc()
+        {
+
+            var exception = await Assert.ThrowsExceptionAsync<ArgumentNullException>(async () =>
+            {
+                _ = await DefaultSendHttpRequestMessage.Instance.SendHttpRequestMessage(
+                    httpClient,
+                    null,
+                    request.Object,
+                    logger.Object,
+                    serializationAdapterMock.Object).ConfigureAwait(false);
+
+            }).ConfigureAwait(false);
+
+            Assert.AreEqual("httpRequestMessageFunc", exception.ParamName);
         }
     }
 }
