f7d308b02f7f33a4e88e27687d20f525ee600be4
Devon Lehman <lehman.devon@gmail.com>
2021-08-11 14:19:58 -0400

    Overload for dependency injection that accepts Action<CreateClientOptions, IServiceProvider>
    Untested, created via github web ui and thus without an IDE to validate that this compiles. See #98

--- src/RestClient.Net.DependencyInjection/RestClientExtensions.cs
+++ src/RestClient.Net.DependencyInjection/RestClientExtensions.cs
@@ -7,23 +7,38 @@ namespace RestClient.Net
 {
     public static class RestClientExtensions
     {
+        private static IServiceCollection AddRestClient_Base(IServiceCollection serviceCollection)
+        {
+            serviceCollection
+                .AddSingleton<CreateHttpClient>((sp) =>
+                {
+                    var microsoftHttpClientFactoryWrapper = new MicrosoftHttpClientFactoryWrapper(sp.GetRequiredService<IHttpClientFactory>());
+                    return microsoftHttpClientFactoryWrapper.CreateClient;
+                })
+                .AddSingleton<CreateClient>((sp) =>
+                {
+                    var clientFactory = new ClientFactory(
+                        sp.GetRequiredService<CreateHttpClient>(),
+                        sp.GetService<ILoggerFactory>());
+
+                    return clientFactory.CreateClient;
+                });
+
+            return serviceCollection;
+        } 
+        
         public static IServiceCollection AddRestClient(this IServiceCollection serviceCollection, Action<CreateClientOptions>? configureClient = null)
         {
-            _ = serviceCollection
-            .AddSingleton<CreateHttpClient>((sp) =>
-            {
-                var microsoftHttpClientFactoryWrapper = new MicrosoftHttpClientFactoryWrapper(sp.GetRequiredService<IHttpClientFactory>());
-                return microsoftHttpClientFactoryWrapper.CreateClient;
-            })
-            .AddSingleton<CreateClient>((sp) =>
-            {
-                var clientFactory = new ClientFactory(
-                    sp.GetRequiredService<CreateHttpClient>(),
-                    sp.GetService<ILoggerFactory>());
+            AddRestClient_Base(serviceCollection);
+            serviceCollection.AddSingleton(sp => sp.GetRequiredService<CreateClient>()("RestClient", configureClient));
 
-                return clientFactory.CreateClient;
-            })
-            .AddSingleton((sp) => sp.GetRequiredService<CreateClient>()("RestClient", configureClient));
+            return serviceCollection;
+        }        
+        
+        public static IServiceCollection AddRestClient(this IServiceCollection serviceCollection, Action<CreateClientOptions, IServicesProvider>? configureClient = null)
+        {
+            AddRestClient_Base(serviceCollection);
+            serviceCollection.AddSingleton(sp => sp.GetRequiredService<CreateClient>()("RestClient", options => configureClient(options, sp)));
 
             return serviceCollection;
         }
